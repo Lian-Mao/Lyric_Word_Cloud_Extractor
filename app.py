f0f28122f54cd502cca634ebfaabea6e28d953fd
Lian Mao Teo <67960963+Lian-Mao@users.noreply.github.com>
2025-08-26 12:55:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -63,5 +63,6 @@ def index():
 
     return render_template('index.html', img_data=img_data, title=title, error=error)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+
+    #app.run(debug=True)
